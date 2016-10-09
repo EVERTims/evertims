aaebf4fa8f1504bc080a650cc408f00571867a03
David Poirier-Quinot <davipoir@gmail.com>
2016-10-09 15:15:25 +0200

    mod evertims start script example (can be launched from anywhere)

--- resources/startEvertims.sh
+++ resources/startEvertims.sh
@@ -1,3 +1,4 @@
 # This is an example starting script for EVERTims
 
-../bin/ims -s 3858 -a 'listener_1/127.0.0.1:3860' -v 'listener_1/localhost:3862' -d 1 -D 2 -m ../resources/materials.dat -p 'listener_1/'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/../bin/ims -s 3858 -a 'listener_1/127.0.0.1:3860' -v 'listener_1/localhost:3862' -d 1 -D 2 -m $DIR/../resources/materials.dat -p 'listener_1/'
