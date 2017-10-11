c210bc538cf0be79a87ca66bb5f833d973891808
David Poirier-Quinot <davipoir@gmail.com>
2017-10-11 16:06:26 +0200

    added build deps to linux build script

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -5,6 +5,14 @@ set -e
 
 BUILDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# install deps
+sudo apt get install git
+sudo apt get install cmake
+# openGL
+sudo apt get install freeglut3-dev
+# bsd
+sudo apt get install libbsd-dev
+
 # ------------------------------------------------------------
 # Build libevert
 # ------------------------------------------------------------
